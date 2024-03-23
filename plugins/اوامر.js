f6073832c2c387b27eb085ed77d8a2ec3e5d0768
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-23 05:00:23 +0000

    Update اوامر.js

--- plugins/اوامر.js
+++ plugins/اوامر.js
@@ -1,4 +1,40 @@
+import { createHash } from 'crypto'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+const time = moment.tz('Egypt').format('HH')
+let wib = moment.tz('Egypt').format('HH:mm:ss')
+
+
+
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => {
+
+
+let d = new Date(new Date + 3600000)
+    let locale = 'ar'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = 
+        global.db.data.users[m.sender];
+    let { min, xp, max } = xpRange(user.level, global.multiplier)
+    let username = conn.getName(who)
+    let math = max - xp
+    let sn = createHash('md5').update(who).digest('hex')
+    let totalreg = Object.keys(global.db.data.users).length;
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+    let more = String.fromCharCode(8206)
+    let readMore = more.repeat(900) 
+    const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
 const caption =`*⌘━──≼━━「🌸」━━≽──━⌘*
 *🌸╎مرحبا بكم في بوت روبين╎🌸*
 *⌘━──≼━━「🌸」━━≽──━⌘*
