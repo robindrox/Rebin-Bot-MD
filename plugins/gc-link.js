ed44dcacc8394f61618bc057e730a744d489cec1
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-25 03:07:46 +0000

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,10 +3,10 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
+      title: 'رابط المجموعة',
+      body: '𝑅𝑂𝐵𝐼𝑁 - 𝐵𝑂𝑇',
       previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
+      sourceUrl: ``}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
