1c711d46495e9bea5315ca95d9650def4f8c65eb
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-27 05:48:01 +0000

    Update gc-revoke.js

--- plugins/gc-revoke.js
+++ plugins/gc-revoke.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn}) => {
   const revoke = await conn.groupRevokeInvite(m.chat);
   await conn.reply(m.chat, `🔹️ *_تمت إعادت تعيين رابط المجموعة بنجاح._*\n♾ • رابط جديد: ${'https://chat.whatsapp.com/' + revoke}`, m);
 };
-handler.command = ['resetlink', 'revoke'];
+handler.command = ['رستر'];
 handler.botAdmin = true;
 handler.admin = true;
 handler.group = true;
