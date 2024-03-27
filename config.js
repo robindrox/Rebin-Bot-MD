1f269902f7a55892640be8aafa25a1d9edf37170
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-27 03:11:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,23 +7,26 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
+global.botnumber = ""
+
 global.owner = [
-  ['212766343649', 'LAW', true],
-  ['212776626688', 'ROBIN', true],
+  ['212717457920', 'bobiza', true],
+
+  ['212717457920','bobiza', true],
+  ['212717457920'],
+  ['212717457920']
 ];
 
-global.xaxa = 'kana'
-global.suittag = ['212766343649', '212766343649'];
-global.prems = ['212766343649', '212766343649'];
+global.suittag = ['212717457920'];
+global.prems = ['212717457920'];
 
-global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
-global.wait = '*[ ⏳ ] Cargando...*';
-
+global.packname = 'bobiza bot';
+global.author = 'bobiza';
+global.wm = 'bobiza';
+global.titulowm = 'bobiza';
+global.titulowm2 = `bobiza`
+global.igfg = 'bobiza';
+global.wait = '*|🏃| loading...*\n*نحاول تلبية طلبكم لا تستعجلوا* ♥\ninstagram.com/noureddine_ouafy';
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
@@ -35,22 +38,22 @@ global.mods = [];
 
 //* *******Tiempo***************
 global.d = new Date(new Date + 3600000);
-global.locale = 'es';
+global.locale = 'en';
 global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
-global.mes = d.toLocaleDateString('es', {month: 'long'});
-global.año = d.toLocaleDateString('es', {year: 'numeric'});
+global.fecha = d.toLocaleDateString('en', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('en', {month: 'long'});
+global.año = d.toLocaleDateString('en', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] Cargando...*';
-global.waittt = '*[ ⏳ ] Cargando...*';
-global.waitttt = '*[ ⏳ ] Cargando...*';
-global.nomorown = '5219993404349';
+global.wm2 = `${dia} ${fecha}\nBOBIZA`;
+global.gt = 'BOBIZA';
+global.mysticbot = 'The BOBIZA';
+global.md = 'https://instagram.com/noureddine_ouafy';
+global.mysticbot = 'https://instagram.com/noureddine_ouafy';
+global.waitt = '*[ ⏳ ] LOADING...*';
+global.waittt = '*[ ⏳ ] LOADING...*';
+global.waitttt = '*[ ⏳ ] LOADING...*';
+global.nomorown = '212605784394';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
