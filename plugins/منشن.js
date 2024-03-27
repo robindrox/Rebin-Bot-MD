08a711062eab246eaca6f459a62b91d9f3a81aba
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-27 18:34:50 +0000

    Update and rename gc-tagall.js to منشن.js

--- plugins/منشن.js
+++ plugins/منشن.js
@@ -5,17 +5,17 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  const oi = `*الرسالة:* ${pesan}`;
+  let teks = `*⺀استدعاء - المجموعة⺀*\n\n❏ ${oi}\n\n❏ *الاعضاء:*\n`;
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝑅𝑂𝐵𝐼𝑁 - 𝐵𝑂𝑇\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(tagall|منشن)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
