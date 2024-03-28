acee1e97d1b7d689d03f6a9876ca2b5f2da5adbb
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-28 04:48:24 +0000

    Create .js

---
+++ plugins/.js
@@ -0,0 +1,17 @@
+
+let handler  = async (m, { conn, args, text }) => {
+if (!text) throw `*أدخل الاسم الذي تريده ليكون اسم المجموعة الجديد*`
+try {
+let text = args.join` `
+if(!args || !args[0]) {
+} else {
+conn.groupUpdateSubject(m.chat, text)}
+} catch (e) {
+throw '*عذرًا، حدث خطأ، لا يمكن أن يزيد الاسم عن 25 حرفًا*'
+}}
+handler.help = ['setname <text>']
+handler.tags = ['group']
+handler.command = /^(الاسم)$/i
+handler.group = true
+handler.admin = true
+export default handler
