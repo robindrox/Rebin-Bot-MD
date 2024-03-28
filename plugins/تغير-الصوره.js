ec166e07af88907487dde5ddd259e82f035ede2c
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-28 04:46:28 +0000

    Update تغير-الصوره.js

--- plugins/تغير-الصوره.js
+++ plugins/تغير-الصوره.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!img) throw '*REPLY TO AN IMAGE.*'
     await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('*تم ضبط الصورة بنجاح كـأفتار المجموعة*'))
     } else throw '*الرد على الصورة.*'}
-    handler.command = /^تغير-الصوره?$/i
+    handler.command = /^تغير-الصوره$/i
     handler.group = true
     handler.admin = true
     handler.botAdmin = true
