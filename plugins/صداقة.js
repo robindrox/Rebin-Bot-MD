3264d00576f34eb2238f453cda28ab604c925d19
robindrox <163097613+robindrox@users.noreply.github.com>
2024-03-29 21:17:13 +0000

    Create صداقة.js

---
+++ plugins/صداقة.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(` *هيا بنا ${toM(a)},لنكن اصدقاء*
+*${toM(b)}, هل توافق لتكونو اصدقاء جدد؟*\n\*ملاحظه ده منشن عشوائي للمرح فقط اذ لم يعجبك الأمر إذن ماتستخدمش الامر💗*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['صداقة']
+handler.group = true
+export default handler
